ff27077d530510875bc6066f0bafb292f7718fb8
marianaquesada <99993176+marianaquesada@users.noreply.github.com>
2024-09-23 12:22:16 -0600

    Tablas terminadas

--- app/database_setup.py
+++ app/database_setup.py
@@ -1,7 +1,8 @@
+#Ultima modificacion: 9/23/24 12:20 am
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base, Mapped, mapped_column, relationship
-
-from sqlalchemy import ForeignKey, Column, Integer, String, DateTime, Boolean
+from sqlalchemy import ForeignKey, Column, Integer, String, DateTime, Boolean, Date, Table
 
 # URL de conexión para PostgreSQL
 DATABASE_URL = 'postgresql+psycopg2://postgres:123@localhost:5432/proyectoAP'
@@ -15,7 +16,12 @@ session = Session()
 
 Base = declarative_base()
 
-
+trip_passengers = Table(
+    'trip_passengers', Base.metadata, 
+    Column('trip_id', ForeignKey('trip.id'), primary_key=True),
+    Column('user_id', ForeignKey('user.id'), primary_key=True),
+    Column('pickup_stop_id', ForeignKey('stop.id'))
+)
 class User(Base):
     __tablename__ = 'user'
 
@@ -26,17 +32,12 @@ class User(Base):
     second_surname = Column(String)
 
     identification = Column(Integer, index=True)
-    birth_date = Column(DateTime)
+    birth_date = Column(Date)
     institutional_email = Column(String, index = True)
     phone_number = Column(String)
-    dl_expiration_date = Column(DateTime)
+    dl_expiration_date = Column(Date)
 
-    # Descripción: Esta línea define un atributo gender_id que actúa como una clave foránea en la tabla de User.
-    # Relación: Este campo referencia el id de la tabla gender, lo que significa que cada usuario puede estar asociado a un género específico.
     gender_id = Column(ForeignKey('gender.id'))
-
-    # Descripción: Cada instancia de User tendrá acceso a la instancia correspondiente de Gender a través de este atributo.
-    # back_populates: Establece que en la clase Gender existe un atributo users que contiene todos los usuarios asociados a ese género.
     gender = relationship('Gender', back_populates="users")
 
     user_type_id = Column(ForeignKey('user_type.id'))
@@ -47,10 +48,9 @@ class User(Base):
     
     trips_as_driver = relationship('Trip', back_populates="driver")
 
-    # Un usuario puede poseer varios vehículos, y este atributo permitirá acceder a todos esos vehículos desde el objeto User.
-    # back_populates: Este parametro establece que en la clase Vehicle existe un atributo owner que apunta a la instancia correspondiente de User
     vehicles = relationship('Vehicle', back_populates="owner")
 
+    trips_as_passenger = relationship("Trip", secondary=trip_passengers, back_populates="passengers")
 
 class Vehicle(Base):
     __tablename__ = 'vehicle'
@@ -63,27 +63,85 @@ class Vehicle(Base):
     owner_id = Column(ForeignKey('user.id'))
     owner = relationship("User", "vehicles")
 
+    vehicle_type_id = Column(ForeignKey('vehicle_type.id'))
+    vehicle_type = relationship('VehicleType', back_populates="vehicles")
+   
+    brand_id = Column(ForeignKey('brand.id'))
+    brand = relationship('Brand', back_populates="vehicles")
+
+    trips = relationship('Trip', 'vehicle')
+
+class VehicleType(Base):
+    __tablename__ = 'vehicle_type'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+    vehicles = relationship('Vehicle', 'vehicle_type')
+
+class Brand(Base):
+    __tablename__  = 'brand'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+    vehicles = relationship("Vehicle", "brand")
+
 class UserType(Base):
     __tablename__ = 'user_type'
     id = Column(Integer, primary_key=True)
     name = Column(String)
 
     users = relationship('User', 'user_type')
-
-class Trip(Base):
+    
+class Trip(Base):  
     __tablename__ = 'trip'
     id = Column(Integer, primary_key=True)
     passenger_limit = Column(Integer)
-    
+    fare_per_person = Column(Integer)
+    route_url = Column(String)
+    departure_datetime = Column(DateTime)
+
     driver_id = Column(ForeignKey('user.id'))
-    driver = relationship("User", "trips_as_driver")
+    driver = relationship("User", back_populates="trips_as_driver")
+
+    starting_point_id = Column(Integer, ForeignKey('stop.id'))
+    starting_point = relationship("Stop", foreign_keys=[starting_point_id])
+
+    finishing_point_id = Column(Integer, ForeignKey('stop.id'))
+    finishing_point = relationship("Stop", foreign_keys=[finishing_point_id])
+    
+    trip_status_id = Column(ForeignKey('trip_status.id'))
+    trip_status = relationship('TripStatus', back_populates="trips")
+
+    vehicle_id = Column(ForeignKey('vehicle.id'))
+    vehicle = relationship('Vehicle', back_populates="trips")
+    
+    passengers = relationship("User", secondary=trip_passengers, back_populates="trips_as_passenger")
+    
+class Stop(Base):
+    __tablename__ = 'stop'
+    id = Column(Integer, primary_key=True)
+    latitude = Column(String)
+    longitude = Column(String)
+    name = Column(String)
+    description = Column(String, nullable=True)
+
+class TripStatus(Base):
+    __tablename__ = 'trip_status'
+    id = Column(Integer, primary_key=True)
+    name = Column(Integer)
+    description = Column(String)
+
+    trips = relationship("Trip", back_populates="trip_status")
+
 
 class Institution(Base):
     __tablename__ = 'institution'
     id = Column(Integer, primary_key=True)
     name = Column(String)
+    description = Column(String)
+    address = Column(String)
 
-    users = relationship("User", "institution")
+    users = relationship("User", back_populates = "institution")
 
 class Gender(Base):
     __tablename__ = 'gender'
@@ -91,7 +149,7 @@ class Gender(Base):
     name = Column(String)
     description = Column(String)
 
-    users = relationship('User', 'gender')
+    users = relationship('User', back_populates = 'gender')
 
 
 Base.metadata.create_all(bind=engine)
