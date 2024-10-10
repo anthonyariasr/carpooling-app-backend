4fe3a08556043b9fff9f6a5712069804f7c9e6ba
Gerardo Gómez B <gerardogomezbrenes2112@gmail.com>
2024-10-10 10:50:24 -0600

    Moved schemas folder into app

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,1 +1,11 @@
-from .user_schema import UserCreate, UserResponse  # Importa los esquemas de user.py
+from .user_schema import UserCreate, UserResponse
+from .vehicle_schema import VehicleCreate, VehicleResponse
+from .vehicle_type_schema import VehicleTypeCreate, VehicleTypeResponse
+from .brand_schema import BrandCreate, BrandResponse
+from .user_type_schema import UserTypeCreate, UserTypeResponse
+from .trip_schema import TripCreate, TripResponse
+from .trip_status_schema import TripStatusCreate, TripStatusResponse
+from .stop_schema import StopCreate, StopResponse
+from .institution_schema import InstitutionCreate, InstitutionResponse
+from .gender_schema import GenderCreate, GenderResponse
+from .trip_passengers_schema import TripPassengerCreate, TripPassengerResponse  # Added import
--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -1,16 +1,41 @@
-from pydantic import BaseModel
-
-# Esquema para crear un usuario
+from pydantic import BaseModel, EmailStr, conint
+from typing import Optional
+from datetime import date
+from decimal import Decimal
 class UserCreate(BaseModel):
     name: str
     lastname: str
+    identification: str
+    second_name: Optional[str] 
+    first_surname: str
+    second_surname: Optional[str] 
+    institutional_email: EmailStr #forma de validar un correo que tiene pydantic
+    phone_number: str
+    birth_date: str
+    gender_id: int
+    user_type_id: int
+    institution_id: int
+    date_registered: date
+    rating: Decimal 
+    total_ratings: int
 
-# Esquema para responder con los datos de un usuario (incluye el ID)
 class UserResponse(BaseModel):
     id: int
     name: str
     lastname: str
+    identification: str
+    second_name: Optional[str] 
+    first_surname: str
+    second_surname: Optional[str] 
+    institutional_email: EmailStr
+    phone_number: str
+    birth_date: str
+    gender_id: int
+    user_type_id: int
+    institution_id: int
+    date_registered: date
+    rating: Decimal 
+    total_ratings: int
 
-    # Permite que Pydantic funcione con objetos SQLAlchemy
     class Config:
         orm_mode = True
